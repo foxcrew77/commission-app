87431a79921436cbbaf028d8a2ac6bf520a2cadc
foxcrew77 <mohdhanif0712@gmail.com>
2024-04-30 15:53:02 +0800

    add migration and model

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function delivery_trips(){
+        return $this->hasMany(Delivery_trip::class);
+    }
 }
